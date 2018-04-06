8ff9e9a4365eec5539cae90deda9560acb4b973d
kovalenko_sa <frowolf@gmail.com>
2018-04-06 12:57:05 +0300

    1

---
+++ app/Http/Controllers/BucketController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Bucket;
+use Illuminate\Http\Request;
+
+class BucketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Bucket  $bucket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bucket $bucket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Bucket  $bucket
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bucket $bucket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Bucket  $bucket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bucket $bucket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Bucket  $bucket
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bucket $bucket)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/bucket.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kovalenko_sa
+ * Date: 06.04.2018
+ * Time: 11:50
+ */
